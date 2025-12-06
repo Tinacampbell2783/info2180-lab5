cfe1e987901f820c4b2ffcea71a75e8bc2de77c4
tinacampbell0803 <Tina.campbell0803@gmail.com>
2025-12-05 21:30:56 -0500

    Completed Exercise 4

--- world.php
+++ world.php
@@ -6,23 +6,36 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-// Read the GET parameter
+// Read GET parameter
 $country = $_GET['country'] ?? "";
 
-// If a country was entered, filter results
+// Build SQL query depending on input
 if (!empty($country)) {
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-} 
-// If empty, return all countries
-else {
+} else {
     $stmt = $conn->query("SELECT * FROM countries");
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-    <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+    <thead>
+        <tr>
+            <th>Country</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= htmlspecialchars($row['name']) ?></td>
+            <td><?= htmlspecialchars($row['continent']) ?></td>
+            <td><?= htmlspecialchars($row['independence_year'] ?: 'N/A') ?></td>
+            <td><?= htmlspecialchars($row['head_of_state'] ?: 'N/A') ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
